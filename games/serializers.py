971d89f29f1247cd7a483ee732182fbb642bd3ab
Park SungHyun <sunghyun0620@gmail.com>
2024-09-10 16:12:30 +0900

    PUT:Games Serializers.py

--- games/serializers.py
+++ games/serializers.py
@@ -3,13 +3,12 @@ from .models import Game, Review, GameCategory, Screenshot
 
 
 class GameListSerializer(serializers.ModelSerializer):
-    star = serializers.FloatField(read_only=True)
     maker_name = serializers.CharField(source='maker.username')
 
     class Meta:
         model = Game
         fields = ("pk", "title", "maker", "thumbnail",
-                  "star", "maker_name", "view_cnt")
+                  "star", "maker_name","content","chip")
 
 
 class GameCreateSerializer(serializers.ModelSerializer):
@@ -32,17 +31,11 @@ class ReviewSerializer(serializers.ModelSerializer):
     author_name = serializers.CharField(
         source='author.username', read_only=True)
     src = serializers.ImageField(source='author.image', read_only=True)
-    replies = serializers.SerializerMethodField()
 
     class Meta:
         model = Review
         fields = "__all__"
         read_only_fields = ('is_visible', 'game', 'author',)
-
-    def get_replies(self, obj):
-        if obj.reply.exists():
-            return ReviewSerializer(obj.reply, many=True).data
-        return []
 
 
 class ScreenshotSerializer(serializers.ModelSerializer):
