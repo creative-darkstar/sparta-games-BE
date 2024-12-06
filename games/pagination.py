6732213b6b2249c643f5fef0052993773f0c5050
Park SungHyun <sunghyun0620@gmail.com>
2024-12-06 16:46:50 +0900

    ADD,Edit:games 디테일 시리얼라이즈 is_liked 추가, 리뷰 정렬과 페이지네이션, 검색-카테고리 API 분리

---
+++ games/pagination.py
@@ -0,0 +1,9 @@
+# pagination.py
+
+from rest_framework.pagination import PageNumberPagination
+
+class CategoryGamesPagination(PageNumberPagination):
+    page_size = 16  # 기본 페이지 크기 설정
+    page_size_query_param = 'limit'  # 클라이언트가 페이지 크기를 조정할 수 있는 파라미터
+    page_query_param = 'page'  # 페이지 번호를 지정하는 쿼리 파라미터
+    max_page_size = 100  # 허용되는 최대 페이지 크기
