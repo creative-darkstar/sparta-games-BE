5916565f24692d09735953d449fd37e46e9c137d
Park SungHyun <sunghyun0620@gmail.com>
2025-01-30 20:12:56 +0900

    Add:settings 미들웨어 및 셋팅에CORS 허용 추가

--- spartagames/custom_middleware.py
+++ spartagames/custom_middleware.py
@@ -3,7 +3,7 @@ from django.utils.deprecation import MiddlewareMixin
 class CustomXFrameOptionsMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         # 로컬 환경의 React 앱 주소를 허용
-        if request.get_host() in ['127.0.0.1:8000', 'localhost:5173', 'sparta-games.net','spartagames-git-dev-horanges-projects.vercel.app']:
+        if request.get_host() in ['127.0.0.1:8000', 'localhost:5173', 'sparta-games.net','spartagames-git-dev-horanges-projects.vercel.app','spartagames-horanges-projects.vercel.app']:
             response['X-Frame-Options'] = 'ALLOWALL'  # 필요에 따라 'DENY'로 변경 가능
         else:
             response['X-Frame-Options'] = 'SAMEORIGIN'  # 기본 값 유지
