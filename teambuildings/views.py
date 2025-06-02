f5f2ef8eef3c141ca997fec765b74a2fbe88c8c9
Park SungHyun <sunghyun0620@gmail.com>
2025-06-02 11:56:35 +0900

    Add:teambuildings 전체 팀빌딩 목록 조회시 유저의 프로필 있는 경우, 없는 경우 추천/마감 게시글 추가와 DB filter의 순서 변경(리스트로 변경하면서 filter를 못쓰는 경우 발생을 생각함)

--- teambuildings/views.py
+++ teambuildings/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from .pagination import TeamBuildPostPagination
 
-from .models import TeamBuildPost
+from .models import TeamBuildPost, TeamBuildProfile
 from .models import ROLE_CHOICES, PURPOSE_CHOICES, DURATION_CHOICES
 from rest_framework import status
 from spartagames.utils import std_response
@@ -31,29 +31,45 @@ class TeamBuildPostAPIView(APIView):
 
     def get(self, request):
         teambuildposts= TeamBuildPost.objects.filter(is_visible=True).order_by('-create_dt')
+        recommendedposts= TeamBuildPost.objects.filter(is_visible=True)
+        user = request.user if request.user.is_authenticated else None
+        
+        # 추천게시글/마감임박 게시글
+        if not user or not user.is_authenticated:
+            # 비회원 유저 : 마감 임박 4개
+            recommendedposts = recommendedposts.order_by('deadline')[:4]
+        #유저 프로필 존재 여부 확인
+        profile=TeamBuildProfile.objects.filter(author=user).first()
+        if profile and user:
+            # 프로필 존재하면 직업 필터
+            my_role= profile.my_role
+            purpose = profile.purpose
+            duration = profile.duration
+
+            # POSTgreSQL용
+            # recommendedposts = recommendedposts.filter(
+            #     want_roles__contains=[my_role],
+            #     purpose=purpose,
+            #     duration=duration
+            # ).orderby('-create_dt')[:4]  # 최대 4개만 추천
+
+            #SQLite용
+            post_list= list(recommendedposts.orderby('-create_dt'))  # 쿼셋 평가
+            filtered_posts = [
+                post for post in post_list 
+                if (
+                    my_role in post.want_roles and
+                    post.purpose == purpose and
+                    post.duration == duration
+                )
+            ]
+            recommendedposts = filtered_posts[:4]  # 최대 4개만 추천
+        else:
+            # 유저 프로필이 없으면 마감 임박 4개
+            recommendedposts = recommendedposts.order_by('deadline')[:4]
 
         if request.query_params.get('status_chip') =="open":
             teambuildposts = teambuildposts.filter(deadline__gte=timezone.now().date())
-        
-        # 유효한 역할코드 목록
-        VALID_ROLE_KEYS = [choice[0] for choice in ROLE_CHOICES]
-        roles_list=list(set(request.query_params.getlist('roles', None)))
-        if roles_list:
-            # 10개 초과 시 에러 반환
-            if len(roles_list) > 10:
-                return std_response(message=f"역할은 최대 10개만 가능합니다.(현재 {len(roles_list)}개)", status="fail", error_code="CLIENT_FAIL", status_code=status.HTTP_400_BAD_REQUEST)
-
-            # 유효하지 않은 role 코드가 포함되면 에러 반환
-            invalid_roles = [role for role in roles_list if role not in VALID_ROLE_KEYS]
-            if invalid_roles:
-                return std_response(message=f"유효하지 않은 역할 코드: {', '.join(invalid_roles)}", status="fail", error_code="CLIENT_FAIL", status_code=status.HTTP_400_BAD_REQUEST)
-            
-            # role 필터링 SQLite에서 JSONField의 contains를 사용X=>POSTgreSQL로 전환이나 N:M 전환 필요
-            #for role in role_list:
-            #    teambuildposts = teambuildposts.filter(want_roles__contains=[role])
-            #SQLite 용 임시
-            posts = list(teambuildposts)  # 쿼셋 평가
-            teambuildposts = [post for post in posts if any(role in post.want_roles for role in roles_list)]
         
         # 유효한 역할코드 목록
         VALID_PURPOSE_KEYS = [p[0] for p in PURPOSE_CHOICES]
@@ -85,12 +101,41 @@ class TeamBuildPostAPIView(APIView):
                 duration_q |= Q(duration=d)
             teambuildposts = teambuildposts.filter(duration_q)
 
+        # 유효한 역할코드 목록
+        VALID_ROLE_KEYS = [choice[0] for choice in ROLE_CHOICES]
+        roles_list=list(set(request.query_params.getlist('roles', None)))
+        if roles_list:
+            # 10개 초과 시 에러 반환
+            if len(roles_list) > 10:
+                return std_response(message=f"역할은 최대 10개만 가능합니다.(현재 {len(roles_list)}개)", status="fail", error_code="CLIENT_FAIL", status_code=status.HTTP_400_BAD_REQUEST)
+
+            # 유효하지 않은 role 코드가 포함되면 에러 반환
+            invalid_roles = [role for role in roles_list if role not in VALID_ROLE_KEYS]
+            if invalid_roles:
+                return std_response(message=f"유효하지 않은 역할 코드: {', '.join(invalid_roles)}", status="fail", error_code="CLIENT_FAIL", status_code=status.HTTP_400_BAD_REQUEST)
+            
+            # role 필터링 SQLite에서 JSONField의 contains를 사용X=>POSTgreSQL로 전환이나 N:M 전환 필요
+            #for role in role_list:
+            #    teambuildposts = teambuildposts.filter(want_roles__contains=[role])
+            #SQLite 용 임시
+            posts = list(teambuildposts)  # 쿼셋 평가
+            teambuildposts = [post for post in posts if any(role in post.want_roles for role in roles_list)]
+        
         #페이지네이션
         paginator = TeamBuildPostPagination()
         paginated_posts = paginator.paginate_queryset(teambuildposts, request)
         serializer = TeamBuildPostSerializer(paginated_posts, many=True)
         response_data= paginator.get_paginated_response(serializer.data).data
-        return std_response(data=response_data["results"],
+
+        # 추천 게시글 직렬화
+        recommended_serializer = TeamBuildPostSerializer(recommendedposts, many=True)
+
+        data={
+            "team_build_posts": response_data["results"],
+            "recommended_posts": recommended_serializer.data
+        }
+
+        return std_response(data=data,
                         message="팀빌딩 목록 불러오기 성공했습니다.", status="success",
                         pagination={"count":response_data["count"], "next":response_data["next"], "previous":response_data["previous"]},
                         status_code=status.HTTP_200_OK)
