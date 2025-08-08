2e1d565fd889aacefebf1f28246254fb885d05e9
Kwan Jeon <jeonkwan100@gmail.com>
2025-08-08 17:29:49 +0900

    Add:alarm alarm브랜치 추가, channels.layers기본 세팅 추가 + requirements

--- spartagames/asgi.py
+++ spartagames/asgi.py
@@ -9,8 +9,21 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spartagames.settings')
 
-application = get_asgi_application()
+# application = get_asgi_application()
+
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    # WebSocket 연결 라우팅을 여기에 추가
+    # "websocket": URLRouter(
+    #     [
+    #         path("ws/...", MyConsumer.as_asgi()),
+    #     ]
+    # ),
+})
