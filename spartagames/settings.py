223127aad11c54d5c970280c544196496666ce7f
Park SungHyun <sunghyun0620@gmail.com>
2025-08-11 22:30:29 +0900

    Edit:settings Celery Beat 작업 중 서비스 원활한 활동을 위해 칩 관련 부분 일시 정지

--- spartagames/settings.py
+++ spartagames/settings.py
@@ -164,26 +164,26 @@ CELERY_TASK_TRACK_STARTED = True
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers.DatabaseScheduler'
 
 CELERY_BEAT_SCHEDULE = {
-    'assign-chips-every-day': {
-        'task': 'games.tasks.assign_chips_to_top_games',
-        'schedule': crontab(hour=4, minute=0),  #crontab(hour=0, minute=0)=>매일 00:00에 실행
-    },
-    'cleanup_new_game_chip':{
-        'task': 'games.tasks.cleanup_new_game_chip',
-        'schedule': timedelta(days=3),
-    },
-    'assign-bookmark-top-chips-daily': {
-        'task': 'games.tasks.assign_bookmark_top_chips',
-        'schedule': crontab(hour=3, minute=45),
-    },
-    'assign-long-play-chips-daily': {
-        'task': 'games.tasks.assign_long_play_chips',
-        'schedule': crontab(hour=3, minute=50),
-    },
-    'assign-review-top-chips-daily': {
-        'task': 'games.tasks.assign_review_top_chips',
-        'schedule': crontab(hour=3, minute=40),
-    },
+    #'assign-chips-every-day': {
+    #    'task': 'games.tasks.assign_chips_to_top_games',
+    #    'schedule': crontab(hour=4, minute=0),  #crontab(hour=0, minute=0)=>매일 00:00에 실행
+    #},
+    #'cleanup_new_game_chip':{
+    #    'task': 'games.tasks.cleanup_new_game_chip',
+    #    'schedule': timedelta(days=3),
+    #},
+    #'assign-bookmark-top-chips-daily': {
+    #    'task': 'games.tasks.assign_bookmark_top_chips',
+    #    'schedule': crontab(hour=3, minute=45),
+    #},
+    #'assign-long-play-chips-daily': {
+    #    'task': 'games.tasks.assign_long_play_chips',
+    #    'schedule': crontab(hour=3, minute=50),
+    #},
+    #'assign-review-top-chips-daily': {
+    #    'task': 'games.tasks.assign_review_top_chips',
+    #    'schedule': crontab(hour=3, minute=40),
+    #},
     'hard-delete-user': {
         'task': 'qnas.tasks.hard_delete_user',
         'schedule': crontab(hour=6, minute=0),
