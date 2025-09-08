0e0b8349557812fbe1a4c4fba3d4564eb9510371
creative-darkstar <creative.darkstar@gmail.com>
2025-09-08 21:37:33 +0900

    Edit: FE 요청사항 반영
    1. 검수 승인 및 반려 시 user 값을 제작자 정보로 하도록 수정. 2. NotificationSerializer의 noti_type 을 label 값으로 하도록 수정. 3. NotificationSerializer 에 content_id 포함

--- commons/serializers.py
+++ commons/serializers.py
@@ -1,10 +1,21 @@
 from rest_framework import serializers
 from .models import Notification
 
+
+class ChoiceDisplayField(serializers.ChoiceField):
+    def to_representation(self, value):
+        # 라벨로 변환해서 응답
+        if value in ('', None):
+            return value
+        return self._choices.get(value, value)
+
+
 class NotificationSerializer(serializers.ModelSerializer):
+    noti_type = ChoiceDisplayField(choices=Notification.NotificationType.choices)
+
     class Meta:
         model = Notification
         fields = [
-            "id", "noti_type", "message",
+            "id", "noti_type", "content_id", "message",
             "is_read", "create_dt"
         ]
