afe904cac0134793de8dc8bc0bfbc0e172b4e83c
creative-darkstar <creative.darkstar@gmail.com>
2026-02-06 21:47:19 +0900

    Add: locust test file

---
+++ server_test/locustfile.py
@@ -0,0 +1,14 @@
+from locust import HttpUser, task, between, TaskSet
+
+
+class UserBehavior(TaskSet):
+    @task
+    def get_user_detail(self):
+        user_id = 1
+        self.client.get(f'users/api/{user_id}')
+
+
+class LocustUser(HttpUser):
+    host = "http://127.0.0.1:8000/"
+    tasks = [UserBehavior]
+    wait_time = between(1, 4)
